1bcd1de36b737915a4f25f9b02834e631dc59fbf
ameysunu <ameysunu00719@gmail.com>
2020-06-28 14:59:32 +0300

    fourth commit

--- lib/water.dart
+++ lib/water.dart
@@ -56,7 +56,7 @@ class _WaterState extends State<Water> {
                 ),
               ),
               Card(
-                color: Hexcolor('#ED4C5C'),
+                color: Hexcolor('#FF864C'),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(25.0),
@@ -75,58 +75,253 @@ class _WaterState extends State<Water> {
                           style: TextStyle(
                               fontFamily: 'Poppins',
                               fontSize: 20.0,
-                              color: Colors.white),
+                              color: Hexcolor('#800080')),
                         ),
                       ),
                     ),
-                    Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                          child: SizedBox(
-                            width: 100.0,
-                            height: 300.0,
-                            child: Text(
-                              'Tap 1',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0,
-                                  color: Colors.white),
-                            ),
+                    Card(
+                      color: Hexcolor('#FF864C'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/tap2.png'),
+                        title: Text(
+                          'Tap1',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#800080'),
                           ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                          child: SizedBox(
-                            width: 100.0,
-                            height: 300.0,
-                            child: Text(
-                              'Tap 2',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0,
-                                  color: Colors.white),
-                            ),
+                        subtitle: Text(
+                          'Last used: 34 minutes ago \n No leakages found.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.done,
+                          color: Hexcolor('#005C19'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#FF864C'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/tap2.png'),
+                        title: Text(
+                          'Tap2',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#800080'),
                           ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                          child: SizedBox(
-                            width: 150.0,
-                            height: 300.0,
-                            child: Text(
-                              'Tap 3',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0,
-                                  color: Colors.white),
-                            ),
+                        subtitle: Text(
+                          'Last used: Leakage detected \n Fix as soon possible',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.not_interested,
+                          color: Hexcolor('#FB020B'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#FF864C'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/shower.png'),
+                        title: Text(
+                          'Shower',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#800080'),
                           ),
                         ),
-                      ],
+                        subtitle: Text(
+                          'Last used: Last used 1h 12min ago \n No leakages found.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.done,
+                          color: Hexcolor('#005C19'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#FF864C'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/toilet.png'),
+                        title: Text(
+                          'Toilet',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#800080'),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Last used: Last used 6min ago \n No leakages found.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.done,
+                          color: Hexcolor('#005C19'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // New Division
+              Card(
+                color: Hexcolor('#C21170'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25.0),
+                    bottom: Radius.circular(25.0),
+                  ),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 0, 20),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'Kitchen',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 20.0,
+                              color: Hexcolor('#FF864C')),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#C21170'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/tap2.png'),
+                        title: Text(
+                          'Main Tap',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#FF864C'),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Leakage found. \n Fix as soon as possible',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.not_interested,
+                          color: Hexcolor('#FB020B'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#C21170'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/tap2.png'),
+                        title: Text(
+                          'Tap2',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#FF864C'),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Last used: 6min ago \n No leakage detected.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.done,
+                          color: Hexcolor('#005C19'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#C21170'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/dish.png'),
+                        title: Text(
+                          'Dishwasher',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#FF864C'),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Last used: Use in progress \n No leakages found.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.done,
+                          color: Hexcolor('#005C19'),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Hexcolor('#C21170'),
+                      elevation: 0,
+                      margin: EdgeInsets.all(5),
+                      child: ListTile(
+                        leading: Image.asset('images/sink.png'),
+                        title: Text(
+                          'Sink',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            color: Hexcolor('#FF864C'),
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Leakage found. \n Fix as soon as possible.',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                        trailing: Icon(
+                          Icons.not_interested,
+                          color: Hexcolor('#FB020B'),
+                        ),
+                      ),
                     ),
                   ],
                 ),
