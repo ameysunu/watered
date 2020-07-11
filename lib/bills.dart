de8552e873c68adda0856e8fc8ac443389985671
ameysunu <ameysunu00719@gmail.com>
2020-07-11 10:33:07 +0300

    bills added

--- lib/bills.dart
+++ lib/bills.dart
@@ -1,216 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'plumber.dart';
-import 'earth.dart';
 
-class Bills extends StatelessWidget {
-  final int number = 03443462020;
-  void call(int number) => launch("tel:$number");
+class Bills extends StatefulWidget {
+  @override
+  _BillsState createState() => _BillsState();
+}
+
+class _BillsState extends State<Bills> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Hexcolor('#ED4C5C'),
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                Card(
-                  color: Hexcolor('#800080'),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.zero,
-                      bottom: Radius.circular(25.0),
-                    ),
-                  ),
-                  child: SizedBox(
-                    height: 300.0,
-                    width: double.infinity,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
-                            child: Text(
-                              'Watered',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 30.0,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
-                            child: Text(
-                              'Water is an important asset.',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(20, 5, 0, 0),
-                            child: Text(
-                              'Do feel free to report any water problems.',
-                              style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontSize: 17.0,
-                                  color: Colors.white70),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(90, 30, 10, 0),
-                            child: ButtonBar(
-                              children: <Widget>[
-                                RaisedButton(
-                                  color: Hexcolor('#FF864C'),
-                                  onPressed: () {
-                                    call(number);
-                                  },
-                                  shape: StadiumBorder(),
-                                  child: Text('Call Water Helpline',
-                                      style: TextStyle(
-                                          fontFamily: 'Poppins',
-                                          fontSize: 20,
-                                          color: Colors.white)),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+    return Scaffold(
+      backgroundColor: Hexcolor('#800080'),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Card(
+                color: Hexcolor('#ED4C5C'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(25.0),
+                    bottom: Radius.circular(25.0),
                   ),
                 ),
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      InkWell(
-                        child: Card(
-                          color: Hexcolor('#800080'),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 10.0),
-                          child: ListTile(
-                            leading: Image.asset('images/plumb.png'),
-                            title: Text(
-                              'Leaking Taps?',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0),
-                            ),
-                            onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => PlumberDetails()),
-                            ),
-                            subtitle: Text(
-                              'Get in touch with a plumber.',
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 15.0),
-                            ),
-                          ),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 0, 20),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'Household Bills',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 30.0,
+                              color: Colors.white),
                         ),
                       ),
-                      InkWell(
-                        child: Card(
-                          color: Hexcolor('#800080'),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 5.0, horizontal: 10.0),
-                          child: ListTile(
-                            leading: Image.asset('images/tree.png'),
-                            title: Text(
-                              'Give back to earth',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0),
-                            ),
-                            onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Earth()),
-                            ),
-                            subtitle: Text(
-                              'Learn how you can save water and trees.',
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 15.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      InkWell(
-                        child: Card(
-                          color: Hexcolor('#800080'),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 10.0),
-                          child: ListTile(
-                            leading: Image.asset('images/donate.png'),
-                            title: Text(
-                              'Donate',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0),
-                            ),
-                            onTap: () async {
-                              if (await canLaunch(
-                                  "https://generosity.org/donate/")) {
-                                await launch("https://generosity.org/donate/");
-                              }
-                            },
-                            subtitle: Text(
-                              'Support water cause by donation.',
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 15.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      InkWell(
-                        child: Card(
-                          color: Hexcolor('#800080'),
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 10.0),
-                          child: ListTile(
-                            leading: Image.asset('images/developer.png'),
-                            title: Text(
-                              'About Developer',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 20.0),
-                            ),
-                            onTap: () async {
-                              if (await canLaunch(
-                                  "https://github.com/ameysunu/")) {
-                                await launch("https://github.com/ameysunu");
-                              }
-                            },
-                            subtitle: Text(
-                              'Developer details.',
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  fontFamily: 'Poppins',
-                                  fontSize: 15.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+                  child: Text(
+                    'Your water bills',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 15.0,
+                        color: Colors.white),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
