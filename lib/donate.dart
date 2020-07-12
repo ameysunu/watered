bdf18eb001fa026ecf47d355f17cbfe7176bb734
ameysunu <ameysunu00719@gmail.com>
2020-07-12 15:17:33 +0300

    donate added

--- lib/donate.dart
+++ lib/donate.dart
@@ -9,74 +9,74 @@ class Donate extends StatefulWidget {
 
 class _DonateState extends State<Donate> {
   final _formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Hexcolor('#ED4C5C'),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(20, 20, 0, 20),
-                    child: Text(
-                      'Donation',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 30.0,
-                          color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(20, 0, 20, 5),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 0, 20),
                   child: Text(
-                    'Even a single dollar can help you change the world.',
+                    'Donation',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Poppins',
-                        fontSize: 15.0,
+                        fontSize: 30.0,
                         color: Colors.white),
                   ),
                 ),
-              ),
-              Card(
-                color: Hexcolor('#FF864C'),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(25.0),
-                    bottom: Radius.circular(25.0),
-                  ),
+              ],
+            ),
+            Container(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 5),
+                child: Text(
+                  'Even a single dollar can help you change the world.',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15.0,
+                      color: Colors.white),
                 ),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 300,
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                        child: Text(
-                          'You are about to initiate a non-refundable payment. Do fill up the below form.',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 15,
-                              color: Colors.white),
-                        ),
+              ),
+            ),
+            Card(
+              color: Hexcolor('#FF864C'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(25.0),
+                  bottom: Radius.circular(25.0),
+                ),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 400,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                      child: Text(
+                        'You are about to initiate a non-refundable payment. Do fill up the below form.',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 15,
+                            color: Hexcolor('#800080')),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            children: <Widget>[
-                              TextFormField(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8.0, 10, 0, 0),
+                              child: TextFormField(
                                 decoration: InputDecoration(
                                   labelText: 'Enter your credit/debit number',
                                   labelStyle: TextStyle(
@@ -90,25 +90,52 @@ class _DonateState extends State<Donate> {
                                     return null;
                                 },
                               ),
-                              RaisedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState.validate()) {
-                                    Scaffold.of(context).showSnackBar(SnackBar(
-                                        content: Text('Processing Data')));
-                                  }
-                                },
-                                child: Text('Submit'),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 10, 0, 0),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Enter CVC number',
+                                  labelStyle: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white),
+                                ),
                               ),
-                            ],
-                          ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8.0, 10, 0, 0),
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Amount (\u0024)',
+                                  labelStyle: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(100, 30, 100, 0),
+                              child: RaisedButton(
+                                color: Hexcolor('#ED4C5C'),
+                                onPressed: null,
+                                child: Text(
+                                  'Donate',
+                                  style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
